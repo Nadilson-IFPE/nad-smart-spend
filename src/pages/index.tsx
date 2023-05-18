cbec7c3ad36066b7cf1c583e1a9a3bb237a9f05b
Nadilson <nadilson@protonmail.com>
2023-05-18 03:29:21 -0300

    Basic interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,60 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hello, world!
-        </p>
+      <div className="">
+        <h1 className="text-center text-white m-0">Gastos</h1>
+        <h2 className="text-center text-white m-0">R$ 10.000,00</h2>
+        <form className="mt-5">
+          <div className="flex gap-[5px] mb-[5px]">
+            <input type="text" placeholder={''} className="mb-[5px] rounded-[5px] bg-transparent white w-full border-2 border-solid border-[#30313D] p-[5px] pt-[2px] pr-[5px]" />
+            <input type="datetime-local" className="bg-gray-500 mb-[5px] rounded-[5px] bg-transparent w-full border-2 border-solid border-[#30313D] p-[5px] pt-[2px] pr-[5px]" />
+          </div>
+          <div className="description">
+            <input type="text" placeholder={'Descreva o gasto...'} className="mb-[5px] rounded-[5px] bg-transparent w-full border-2 border-solid border-[#30313D] p-[5px] pt-[2px] pr-[5px]" />
+          </div>
+          <button type="submit" className="w-full mt-[5px] border-0 rounded-[5px] p-[5px] bg-gray-300 hover:bg-gray-500 text-black">Adicionar novo gasto</button>
+        </form>
+        <div className="mt-[10px]">
+          <div className="flex pt-[5px] pr-0 justify-between mb-2 border-t border-solid border-[#30313D] first:border-t-0">
+            <div className="text-left">
+              <div className="text-base">Celular</div>
+              <div className="text-xs text-gray-400">Informações sobre o gasto</div>
+            </div>
+            <div className="text-right">
+              <div className="text-red-600">-R$ 1.000.00</div>
+              <div className="text-xs text-gray-500">22/03/1973</div>
+            </div>
+          </div>
 
+          <div className="flex pt-[5px] pr-0 justify-between mb-2 border-t border-solid border-[#30313D] first:border-t-0">
+            <div className="text-left">
+              <div className="text-base">Manutenção de PC</div>
+              <div className="text-xs text-gray-400">Informações gerais</div>
+            </div>
+            <div className="text-right">
+              <div className="text-green-600">R$ 100,00</div>
+              <div className="text-xs text-gray-500">22/03/2023</div>
+            </div>
+          </div>
+
+          <div className="flex pt-[5px] pr-0 justify-between mb-2 border-t border-solid border-[#30313D] first:border-t-0">
+            <div className="text-left">
+              <div className="text-base">TV LG</div>
+              <div className="text-xs text-gray-400">Informações sobre o gasto</div>
+            </div>
+            <div className="text-right">
+              <div className="text-red-600">-R$ 2.600.00</div>
+              <div className="text-xs text-gray-500">22/03/2023</div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </main>
-  )
+  );
 }
